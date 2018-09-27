a45a4b6357f73070d6626014f8c3b075cb7188b0
jashelton <justin.shelton@yahoo.com>
2018-09-27 19:27:23 -0400

    removing ImageViewer close icon... switch to close on press

--- Components/ImageViewerComponent.js
+++ Components/ImageViewerComponent.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { StyleSheet, Modal, TouchableHighlight } from "react-native";
+import { Modal } from "react-native";
 import ImageViewer from "react-native-image-zoom-viewer";
-import { Icon } from "react-native-elements";
 
-const ImageViewerComponent = props => {
+export default ImageViewerComponent = props => {
   const { visible, image, onClose } = props;
   return (
     <Modal
@@ -18,27 +17,8 @@ const ImageViewerComponent = props => {
         enableSwipeDown={true}
         renderIndicator={() => null}
         saveToLocalByLongPress={false}
-        renderHeader={() => (
-          <TouchableHighlight
-            style={styles.headerStyles}
-            onPress={() => onClose()}
-          >
-            <Icon name="close" size={38} color="#fff" />
-          </TouchableHighlight>
-        )}
+        onClick={() => onClose()}
       />
     </Modal>
   );
 };
-
-const styles = StyleSheet.create({
-  headerStyles: {
-    position: "absolute",
-    width: "100%",
-    padding: 15,
-    alignItems: "flex-end",
-    zIndex: 10000
-  }
-});
-
-export default ImageViewerComponent;
